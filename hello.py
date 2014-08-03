4e3767e7ce3f66138d0669d01bebb6ab5fee4b77
gdevanla <grdvnl@gmail.com>
2014-08-02 20:09:13 -0700

    update

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ client = Client(
     consumer_key="0d6f512bc947f1aaa02786d849bccd09c1129340",
     consumer_secret="c10ed411b904bc5969b8535317ed93dcce20a7ad",
     #callback_url="oob"
-    callback_url="http://thawing-savannah-5714.herokuapp.com/ocb?method=addmodel"
+    callback_url="http://thawing-savannah-5714.herokuapp.com/ocb"
 )
 
 app = Flask(__name__)
@@ -37,8 +37,13 @@ def ocb():
                   request.args.get('oauth_verifier'))
     session['oauth_token'] = request.args.get('oauth_token')
     session['oauth_verifier'] = request.args.get('oauth_verifier')
-    print request.args.get('method')
-    return redirect(url_for(request.args.get('method')))
+
+    print "done verifying"
+    info = client.get_materials()
+    print "Info = ", info
+    #return make_response()
+    return make_response(json.JSONEncoder().encode(info))
+
 
 @app.route("/addmodel", methods=['GET'])
 def addmodel():
