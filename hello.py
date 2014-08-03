833682bf8d0afbd33cb37be82dd8d9b94d0bb1ac
gdevanla <grdvnl@gmail.com>
2014-08-02 17:48:09 -0700

    update

--- hello.py
+++ hello.py
@@ -4,13 +4,14 @@ from flask import request
 from flask import make_response
 import json
 import yaml
+from flask import redirect, url_for
 
 from shapeways.client import Client
 
 client = Client(
     consumer_key="0d6f512bc947f1aaa02786d849bccd09c1129340",
     consumer_secret="c10ed411b904bc5969b8535317ed93dcce20a7ad",
-    callback_url="http://thawing-savannah-5714.herokuapp.com/oauth-callback"
+    callback_url="http://thawing-savannah-5714.herokuapp.com/ocb"
 )
 
 
@@ -30,17 +31,22 @@ def add_model():
         "acceptTermsAndConditions": True,
     }
 
-    client.connect()
+    r = client.connect()
 
-    r = client.add_model_file(modelId, params)
-    print r
+    #r = client.add_model_file(modelId, params)
+    #print r
+    return redirect(r)
+    #return redirect('http://localhost:5000/?oauth_token=393ddd84b2b38d7e367a76a648633b47d45e091d&oauth_verifier=15daba
 
     #return make_response(r)
-    return make_response(json.JSONEncoder().encode([]))
+    #return make_response(json.JSONEncoder().encode([]))
 
 @app.route('/ocb', methods=['GET'])
-def oauth_cb():
-    return make_response(json.JSONEncoder().encode(["calledback"]))
+def ocb():
+    client.verify(request.args)
+    info = client.get_api_info()
+    return make_response(info)
+    #return make_response(json.JSONEncoder().encode(["calledback"]))
 
 # if __name__ == '__main__':
 #     app.run()
