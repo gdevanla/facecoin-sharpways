81e37681d410ea20e803e9fc3d1cd72162bd2c31
gdevanla <grdvnl@gmail.com>
2014-08-02 19:59:32 -0700

    update

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import request
 from flask import make_response
 import json
 import yaml
-from flask import redirect, url_for
+from flask import redirect, url_for, session
 
 from shapeways.client import Client
 
@@ -12,23 +12,43 @@ client = Client(
     consumer_key="0d6f512bc947f1aaa02786d849bccd09c1129340",
     consumer_secret="c10ed411b904bc5969b8535317ed93dcce20a7ad",
     #callback_url="oob"
-    callback_url="http://localhost:5000/ocb"
-    #callback_url="http://thawing-savannah-5714.herokuapp.com/ocb"
+    callback_url="http://thawing-savannah-5714.herokuapp.com/ocb?method=addmodel"
 )
-
 
 app = Flask(__name__)
 app.debug = True
-
-oauth_token='f5ae6c8dd25d67a983b3aa87686a30cbaeb94326'
-oauth_verifier='b35006'
+app.secret_key = 'why would I tell you my secret key?'
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-@app.route('/addmodel', methods=['GET'])
-def add_model():
+@app.route('/auth', methods=['GET'])
+def auth():
+    print 'In auth....'
+    r = client.connect()
+    print r
+    return redirect(r)
+
+@app.route('/ocb', methods=['GET'])
+def ocb():
+
+    client.verify(request.args.get('oauth_token'),
+                  request.args.get('oauth_verifier'))
+    session['oauth_token'] = request.args.get('oauth_token')
+    session['oauth_verifier'] = request.args.get('oauth_verifier')
+    print request.args.get('method')
+    return redirect(url_for(request.args.get('method')))
+
+@app.route("/addmodel", methods=['GET'])
+def addmodel():
+    if 'oauth_verifier' not in session:
+        print 'Calling auth'
+        client.callback_url = "oob" #client.callback_url % ('addmodel')
+        auth()
+        return make_response(json.JSONEncoder().encode([]))
+
+    client.verify(session['oauth_token'], session['oauth_verifier'])
     modelId = 42
     params = {
         "file": "test",
@@ -36,26 +56,6 @@ def add_model():
         "hasRightsToModel": True,
         "acceptTermsAndConditions": True,
     }
-
-    r = client.connect()
-
-    #r = client.add_model_file(modelId, params)
-    print r
-    return redirect(r)
-    #return redirect('http://localhost:5000/?oauth_token=393ddd84b2b38d7e367a76a648633b47d45e091d&oauth_verifier=15daba
-
-    #return make_response(r)
-    #return make_response(json.JSONEncoder().encode([]))
-
-@app.route('/ocb', methods=['GET'])
-def ocb():
-    #print request.args
-    #print request.args.get('oauth_token'), request.args.get('oauth_verifier')
-    client.verify(request.args.get('oauth_token'), request.args.get('oauth_verifier'))
-    # oauth_token='a1fb6f511c6a2186eb466b3699e12b5a1b715494'
-    # oauth_verifier='d6e30d'
-    # print oauth_token , oauth_verifier
-    # client.verify(oauth_token, oauth_verifier)
 
     print "done verifying"
     info = client.get_materials()
